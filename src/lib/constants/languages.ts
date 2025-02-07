1473764d00f8cbc5c0a2031e63ac1faae6b4a621
DeshanniLiyanage <92189179+DeshanniLiyanage@users.noreply.github.com>
2025-02-08 05:26:31 +0900

    restructuring folders. introducing lib folder -folder/api -folder/constants -folder/types -folder/utils

---
+++ src/lib/constants/languages.ts
@@ -0,0 +1,6 @@
+export const LANGUAGES = [
+	{ code: 'fr', name: 'French' },
+	{ code: 'es', name: 'Spanish' },
+	{ code: 'de', name: 'German' },
+	{ code: 'it', name: 'Italian' }
+];
---
+++ src/lib/constants/topics.ts
@@ -0,0 +1,6 @@
+export const TOPICS = [
+  { id: "greetings", name: "Greetings" },
+  { id: "food", name: "Food" },
+  { id: "travel", name: "Travel" },
+  { id: "work", name: "Work" },
+];
